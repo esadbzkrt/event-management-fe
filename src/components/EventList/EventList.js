99d24159d0aea5f786dbd0545404eb239575f826
esadbzkrt <esadbzkrt@gmail.com>
2022-11-26 17:59:47 +0300

    Get Request

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,8 +1,44 @@
 import './EventList.scss'
+import SingleEvent from "./SingleEvent/SingleEvent";
+import {IoMdAddCircleOutline} from 'react-icons/io';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function EventList() {
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        getEvents();
+    }, []);
+
+
+    const getEvents = () => {
+        axios.get('http://localhost:8080/events')
+            .then(response => {
+                setEvents(response.data);
+                console.log(response.data);
+            }
+        );
+    }
+
+
     return (
-        <div>Event List</div>
+        <div className="event-list">
+            <div className="event-list-header">
+                <h3>Event List</h3>
+                <IoMdAddCircleOutline size="24px"/>
+            </div>
+            <div className="event-list-header">
+                <h3>Event Id</h3>
+                <p>Event Name</p>
+                <p>Event Start Date</p>
+                <p>Event End Date</p>
+                <p>Event Capacity</p>
+            </div>
+            {events.map(event => <SingleEvent key={event.id} event={event}/>)}
+
+        </div>
     );
 }
 
--- src/components/EventList/SingleEvent/SingleEvent.js
+++ src/components/EventList/SingleEvent/SingleEvent.js
@@ -1,8 +1,14 @@
 import './SingleEvent.scss';
 
-function SingleEvent() {
+function SingleEvent(props) {
     return (
-        <div> Single Event</div>
+        <div className="event">
+            <h3>{props.event.id}</h3>
+            <p>{props.event.eventName}</p>
+            <p>{props.event.eventStartDate}</p>
+            <p>{props.event.eventEndDate}</p>
+            <p>{props.event.participantsCount} / {props.event.eventCapacity} </p>
+        </div>
     );
 }
 
