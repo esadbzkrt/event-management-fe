195b923b2974f398bb09fab931ed8160acae9af7
esadbzkrt <esadbzkrt@gmail.com>
2022-11-27 16:31:33 +0300

    Context Api

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -3,6 +3,7 @@ import SingleEvent from "./SingleEvent/SingleEvent";
 import {IoMdAddCircleOutline} from 'react-icons/io';
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {MainContext} from "../../context";
 
 function EventList() {
 
@@ -12,33 +13,31 @@ function EventList() {
         getEvents();
     }, []);
 
-
     const getEvents = () => {
         axios.get('http://localhost:8080/events')
             .then(response => {
-                setEvents(response.data);
-                console.log(response.data);
-            }
-        );
+                    setEvents(response.data);
+                    console.log(response.data);
+                }
+            );
     }
 
+    const data = {
+        events,
+        setEvents,
+        getEvents
+    };
 
     return (
-        <div className="event-list">
-            <div className="event-list-header">
-                <h3>Event List</h3>
-                <IoMdAddCircleOutline size="24px"/>
+        <MainContext.Provider value={data}>
+            <div className="event-list">
+                <div className="event-list-header">
+                    <h3>Event List</h3>
+                    <IoMdAddCircleOutline size="24px"/>
+                </div>
+                {events.map(event => <SingleEvent key={event.id} event={event}/>)}
             </div>
-            <div className="event-list-header">
-                <h3>Event Id</h3>
-                <p>Event Name</p>
-                <p>Event Start Date</p>
-                <p>Event End Date</p>
-                <p>Event Capacity</p>
-            </div>
-            {events.map(event => <SingleEvent key={event.id} event={event}/>)}
-
-        </div>
+        </MainContext.Provider>
     );
 }
 
--- src/components/EventList/SingleEvent/SingleEvent.js
+++ src/components/EventList/SingleEvent/SingleEvent.js
@@ -1,13 +1,29 @@
 import './SingleEvent.scss';
+import {AiOutlineDelete,AiOutlineEdit} from 'react-icons/ai';
+import axios from "axios";
+import {MainContext,useContext} from "../../../context";
 
 function SingleEvent(props) {
+
+    const {getEvents} = useContext(MainContext);
+
+    const deleteEvent = () => {
+        axios.delete(`http://localhost:8080/events/${props.event.id}`)
+            .then(response => {
+                console.log(response);
+                getEvents();
+            });
+    }
+
+
     return (
         <div className="event">
-            <h3>{props.event.id}</h3>
-            <p>{props.event.eventName}</p>
+            <h3>{props.event.eventName}</h3>
             <p>{props.event.eventStartDate}</p>
             <p>{props.event.eventEndDate}</p>
             <p>{props.event.participantsCount} / {props.event.eventCapacity} </p>
+            <AiOutlineDelete size="24px" onClick={deleteEvent}/>
+            <AiOutlineEdit size="24px"/>
         </div>
     );
 }
